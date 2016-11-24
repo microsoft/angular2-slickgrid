293fca7eb0193c7b7c06232e6726e28074a96721
Anthony Dresser <andresse@microsoft.com>
2016-11-23 18:06:46 -0800

    updated compiled files

--- components/js/SlickGrid.js
+++ components/js/SlickGrid.js
@@ -333,7 +333,9 @@ let SlickGrid = SlickGrid_1 = class SlickGrid {
         }
     }
     getSelectedRanges() {
-        return this._gridSyncService.selectionModel.getSelectedRanges();
+        if (this._gridSyncService && this._gridSyncService.selectionModel) {
+            return this._gridSyncService.selectionModel.getSelectedRanges();
+        }
     }
     registerPlugin(plugin) {
         if (Slick[plugin] && typeof Slick[plugin] === 'function') {
@@ -346,7 +348,9 @@ let SlickGrid = SlickGrid_1 = class SlickGrid {
     }
     setActive() {
         this._grid.setActiveCell(0, 1);
-        this._gridSyncService.selectionModel.setSelectedRanges([new Slick.Range(0, 0, 0, 0)]);
+        if (this._gridSyncService && this._gridSyncService.selectionModel) {
+            this._gridSyncService.selectionModel.setSelectedRanges([new Slick.Range(0, 0, 0, 0)]);
+        }
     }
     set selection(range) {
         if (typeof range === 'boolean') {
@@ -386,13 +390,13 @@ let SlickGrid = SlickGrid_1 = class SlickGrid {
             if (this.selectionModel) {
                 if (Slick[this.selectionModel] && typeof Slick[this.selectionModel] === 'function') {
                     this._gridSyncService.underlyingSelectionModel = new Slick[this.selectionModel]();
+                    this._grid.setSelectionModel(this._gridSyncService.selectionModel);
                 }
                 else {
                     console.error(`Tried to register selection model ${this.selectionModel}, but none was found to be attached to Slick Grid or it was not a function.
                                 Please extend the Slick with the selection model as a function before registering`);
                 }
             }
-            this._grid.setSelectionModel(this._gridSyncService.selectionModel);
             this._gridSyncService.scrollBarWidthPX = this._grid.getScrollbarDimensions().width;
             this._gridSyncSubscription = this._gridSyncService.updated
                 .filter(p => p === 'columnWidthPXs')
