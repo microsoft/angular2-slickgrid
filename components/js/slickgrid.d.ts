f0f411bf536aecd7e74aa4f26e0358d9f0fbf3c4
Anthony Dresser <andresse@microsoft.com>
2016-12-01 10:58:38 -0800

    fixed capitlization

---
+++ components/js/slickgrid.d.ts
@@ -0,0 +1,73 @@
+import { OnChanges, OnInit, OnDestroy, SimpleChange, EventEmitter, AfterViewInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { IObservableCollection, IGridDataRow, IColumnDefinition } from './interfaces';
+import { ISlickRange } from './selectionmodel';
+export declare class SlickGrid implements OnChanges, OnInit, OnDestroy, AfterViewInit {
+    private _el;
+    private _gridSyncService;
+    columnDefinitions: IColumnDefinition[];
+    dataRows: IObservableCollection<IGridDataRow>;
+    resized: Observable<any>;
+    editableColumnIds: string[];
+    highlightedCells: {
+        row: number;
+        column: number;
+    }[];
+    blurredColumns: string[];
+    contextColumns: string[];
+    columnsLoading: string[];
+    overrideCellFn: (rowNumber, columnId, value?, data?) => string;
+    showHeader: boolean;
+    showDataTypeIcon: boolean;
+    enableColumnReorder: boolean;
+    enableAsyncPostRender: boolean;
+    selectionModel: string;
+    plugins: string[];
+    loadFinished: EventEmitter<void>;
+    cellChanged: EventEmitter<{
+        column: string;
+        row: number;
+        newValue: any;
+    }>;
+    editingFinished: EventEmitter<any>;
+    contextMenu: EventEmitter<{
+        x: number;
+        y: number;
+    }>;
+    topRowNumber: number;
+    topRowNumberChange: EventEmitter<number>;
+    onFocus(): void;
+    private _grid;
+    private _gridColumns;
+    private _gridData;
+    private _rowHeight;
+    private _resizeSubscription;
+    private _gridSyncSubscription;
+    private _topRow;
+    private _leftPx;
+    private static getDataWithSchema(data, columns);
+    constructor(_el: any, _gridSyncService: any);
+    ngOnChanges(changes: {
+        [propName: string]: SimpleChange;
+    }): void;
+    private invalidateRange(start, end);
+    ngOnInit(): void;
+    ngAfterViewInit(): void;
+    ngOnDestroy(): void;
+    onResize(): void;
+    getSelectedRanges(): ISlickRange[];
+    registerPlugin(plugin: string): void;
+    setActive(): void;
+    selection: ISlickRange[] | boolean;
+    private getColumnEditor;
+    private getFormatter;
+    private initGrid();
+    private subscribeToScroll();
+    private subscribeToCellChanged();
+    private updateColumnWidths();
+    subscribeToContextMenu(): void;
+    private updateSchema();
+    private getImagePathForDataType(type);
+    private setCallbackOnDataRowsChanged();
+    private renderGridDataRowsRange(startIndex, count);
+}
---
+++ index.d.ts
@@ -0,0 +1,5 @@
+export * from './components/js/gridsync.service';
+export * from './components/js/interfaces';
+export * from './components/js/selectionmodel';
+export * from './components/js/slickgrid';
+export * from './components/js/virtualizedcollection';
