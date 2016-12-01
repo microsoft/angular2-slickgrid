8910cc592ac113ea798711171e4f1a2393e0d50a
Anthony Dresser <andresse@microsoft.com>
2016-12-01 10:58:01 -0800

    fixing capitalization

--- index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './components/js/gridsync.service';
-export * from './components/js/interfaces';
-export * from './components/js/selectionmodel';
-export * from './components/js/slickgrid';
-export * from './components/js/virtualizedcollection';
